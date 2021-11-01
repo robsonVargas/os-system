8a10d6cc6303b4c84180d8fbc92b771da6bb9164
Robson Vargas <robsonsvargas1993@gmail.com>
2021-11-01 06:21:42 -0300

    Criada a classe OS

---
+++ src/main/java/com/robson/os/domain/OS.java
@@ -0,0 +1,119 @@
+package com.robson.os.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.robson.os.domain.enuns.Prioridade;
+import com.robson.os.domain.enuns.Status;
+
+public class OS {
+	private Integer id;
+	private LocalDateTime dataAbertura;
+	private LocalDateTime dataFechamento;
+	private Integer prioridade;
+	private String observações;
+	private Integer status;
+	private Tecnico tecnico;
+	private Cliente cliente;
+	
+	public OS() {
+		super();
+		this.setDataAbertura(LocalDateTime.now());
+		this.setPrioridade(Prioridade.BAIXA);
+		this.setStatus(Status.ABERTO);
+	}
+
+	public OS(Integer id, Prioridade prioridade, String observações, 
+			Status status, Tecnico tecnico, Cliente cliente) {
+		super();
+		this.id = id;
+		this.setDataAbertura(LocalDateTime.now());
+		this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+		this.observações = observações;
+		this.status = (status == null) ? 0 : status.getCod();
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade.getCod();
+	}
+
+	public String getObservações() {
+		return observações;
+	}
+
+	public void setObservações(String observações) {
+		this.observações = observações;
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Status status) {
+		this.status = status.getCod();
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OS other = (OS) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
