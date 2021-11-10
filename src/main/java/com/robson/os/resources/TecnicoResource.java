26b79cfa32cd7d150de79e4ed46716074a790324
Robson Vargas <robsonsvargas1993@gmail.com>
2021-11-10 12:39:33 -0300

    exceção para objeto não encontrado

--- src/main/java/com/robson/os/resources/TecnicoResource.java
+++ src/main/java/com/robson/os/resources/TecnicoResource.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.robson.os.domain.Tecnico;
 import com.robson.os.dtos.TecnicoDTO;
 import com.robson.os.services.TecnicoService;
 
